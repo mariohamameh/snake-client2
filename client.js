4b13f2a922b6181b8952fe778d2ab77f8865f60e
mariohamameh <mariohamameh97@gmail.com>
2022-03-17 01:17:56 +0000

    refactoring

--- client.js
+++ client.js
@@ -1,26 +1,32 @@
 const net = require("net");
+const { markAsUntransferable } = require("worker_threads");
 
+const { IP, PORT } = require("./constants");
 // establishes a connection with the game server
 const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",// IP address here,
-    port: 50541// PORT number here,
-  });
+    const conn = net.createConnection({
+      host: 'localhost',// IP address here,
+      port: 50541// PORT number here,
+    });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("connect", () => {
-    // code that does something when the connection is first established
-    console.log("somebody just joined!");
-    conn.write("Name: MAR");
-  });
-  return conn;
-};
-conn.on("data", (data) => {
-    // code that does something when the connection is first established
-    console.log('server says', data);
-  });
-
+    // interpret incoming data as text
+    conn.setEncoding("utf8");
+    conn.on("connect", () => {
+      // code that does something when the connection is first established
+      console.log("somebody just joined!");
+      conn.write("Name: MAR");
+    });
+    
+    conn.on('data', (data) => {
+          //console.log("Move: up");
+          console.log('Server says: ', data);
+          conn.end();
+        });
+    conn.on('end', () => {
+          console.log('disconnected from server');
+    });
+    return conn;
+  };
 console.log("Connecting ...");
 connect();
 
