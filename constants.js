3050b2afd55ff1d1d45f33ed3d7d54774ee8a2a5
mariohamameh <mariohamameh97@gmail.com>
2022-03-17 01:07:48 +0000

    added constants file

---
+++ constants.js
@@ -0,0 +1,26 @@
+const IP = 'localhost';
+const PORT = 50541;
+const NAME = "MAR";
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd';
+const MESSAGES = {
+  '1': 'Say: Loool',
+  '2': 'Say: damn im good at this!',
+  '3': 'Say: HANGRY SNAAAAKE!',
+  '4': 'Say: ❤️❤️❤️❤️❤️❤️❤️❤️❤️',
+  '5': 'Say: loooool you ded!'
+};
+
+//EXPORT
+module.exports = {
+  IP,
+  PORT,
+  NAME,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES
+};
